3225ecec12f8169a68fd47e4423c97358ef2adb3
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-03-31 16:24:52 +0900

    Initial Commit

---
+++ Struct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Struct
+//
+//  Created by Kenta Yamada on 2021/03/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
